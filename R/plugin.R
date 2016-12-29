e8e2b9e7eb1dbfa4fcdd5d995e3c9900b80c73e1
GuillermoGonzalezSantander <guillermo.gonzalez@baobabsoluciones.es>
2016-12-29 16:58:11 +0100

    Changes to code to make it work

--- R/plugin.R
+++ R/plugin.R
@@ -21,8 +21,8 @@ gurobi_status_code <- list(
   INPROGRESS = 14L
   )
 
-solve_OP <- function(x, control) {
-  solver <- ROI:::get_solver_name( getPackageName() )
+solve_OP <- function(x, control=list()) {
+  solver <- .ROI_plugin_get_solver_name( getPackageName() )
 
   model <- list()
   model$A <- constraints(x)$L
@@ -30,90 +30,101 @@ solve_OP <- function(x, control) {
   model$modelsense <- if(x$maximum) "max" else "min"
   model$rhs <- constraints(x)$rhs
   model$sense <- constraints(x)$dir
+  model$sense <- sub(x=model$sense, pattern="==", replacement="=")
   model$vtype <- types(x)
 
-  out <- gurobi(model, control)
+  out <- gurobi(model)
 
-  ROI:::canonicalize_solution( solution = out$x,
-                             optimum = out$objval,
-                             status = gurobi_status_code[[out$status]],
-                             solver = solver )
+  .ROI_plugin_canonicalize_solution( solution = out$x,
+									optimum = out$objval,
+									status = gurobi_status_code[[out$status]],
+									solver = solver )
 }
 
 
 .add_status_codes <- function() {
-  solver <- ROI:::get_solver_name( getPackageName() )
-  ROI:::add_status_code_to_db(solver,
+  solver <- .ROI_plugin_get_solver_name( getPackageName() )
+  .ROI_plugin_add_status_code_to_db(solver,
                               1L,
                               "LOADED",
                               "Model is loaded, but no solution information is available."
                             )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               2L,
                               "OPTIMAL",
                               "Model was solved to optimality (subject to tolerances), and an optimal solution is available.",
                               0L # this is for roi_code. 0 means we have got the solution. 1L is the default value.
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               3L,
                               "INFEASIBLE",
                               "Model was proven to be infeasible."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               4L,
                               "INF_OR_UNBD",
                               "Model was proven to be either infeasible or unbounded."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               5L,
                               "UNBOUNDED",
                               "Model was proven to be unbounded."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               6L,
                               "CUTOFF",
                               "Optimal objective for model was proven to be worse than the value specified in the Cutoff parameter. No solution information is available."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               7L,
                               "ITERATION_LIMIT",
                               "Optimization terminated because the total number of simplex iterations performed exceeded the value specified in the IterationLimit parameter, or because the total number of barrier iterations exceeded the value specified in the BarIterLimit parameter."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               8L,
                               "NODE_LIMIT",
                               "Optimization terminated because the total number of branch-andcut nodes explored exceeded the value specified in the NodeLimitparameter."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               9L,
                               "TIME_LIMIT",
                               "Optimization terminated because the time expended exceeded the value specified in the TimeLimit parameter."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               10L,
                               "SOLUTION_LIMIT",
                               "Optimization terminated because the number of solutions found reached the value specified in the SolutionLimit parameter."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               11L,
                               "INTERRUPTED",
                               "Optimization was terminated by the user."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               12L,
                               "NUMERIC",
                               "Optimization was terminated due to unrecoverable numerical difficulties."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               13L,
                               "SUBOPTIMAL",
                               "Unable to satisfy optimality tolerances; a sub-optimal solution is available."
                               )
-  ROI:::add_status_code_to_db(solver,
+  .ROI_plugin_add_status_code_to_db(solver,
                               14L,
                               "INPROGRESS",
                               "An asynchronous optimization call was made, but the associated optimization run is not yet complete."
                               )
 
   invisible(TRUE)
+}
+
+.add_controls <- function(){
+    solver <- .ROI_plugin_get_solver_name( getPackageName() )
+    ## GLPK
+    .ROI_plugin_register_solver_control( solver,
+                                        "OutputFlag",
+                                        "verbose" )
+    
+    invisible( TRUE )
 }
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Gurobi plug in for R package: ROI
 
-[ROI](https://github.com/cran/ROI) is an R package that creates one interface for multiple optimization solvers. It provides great convenience to R users who want to switch different solvers easily for their optimization problems. To add a new solver to the ROI framework, one needs to create a plugin for the new solver. Currently, the available solver plugins on github are: *glpk*, *symphony* and *quadprog*. 
+[ROI](https://github.com/cran/ROI) is an R package that creates one interface for multiple optimization solvers. It provides great convenience to R users who want to switch different solvers easily for their optimization problems. To add a new solver to the ROI framework, one needs to create a plugin for the new solver. Currently, the available solver plugins on github are: *glpk*, *symphony* and *quadprog*.
 
 
 This plugin is written for one of the leading commercial optimization solver [Gurobi](http://gurobi.com/)'s R API. Since I am using  **Gurobi 6.5.0**, to be compatible, your R version needs to be **>= R 3.2.1**. 
 
 
 This plugin hasn't been tested extensively and is not submitted to CRAN yet, thus I recommend that you install it by
- - `devtools::install_github('kuol/ROI.plugin.gurobi')`
+ - `devtools::install_github('ggsdc/ROI.plugin.gurobi')`
 
 The plugin will be included automatically when you include the package *ROI*, so you only need to 
 ```
@@ -17,3 +17,8 @@ in order to use it. You can verify if *ROI.plugin.gurobi* is registered to *ROI*
 ```
 ROI_registered_solvers()
 ```
+
+
+This repository is a forked from: [original repository](https://github.com/kuol/ROI.plugin.gurobi).
+
+It has been modified in order to work with the latest ROI version and it is being updated to include more parameter options from gurobi in the connection.
